d1d1f72751651c58fb3ba7442afd68214cab69d9
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-13 11:16:49 +1100

    redux put into todolist

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {removeTodo} from "./actions";
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 
-const TodoList = ({ todos = [] }) => (
+const TodoList = ({todos = [], onRemovedPressed}) => (
     <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} />)}
+        <NewTodoForm/>
+        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovedPressed}/>)}
     </div>
 );
 
-export default TodoList;
+const mapStateToProps = state => ({
+    todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemovedPressed: text => dispatch(removeTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
